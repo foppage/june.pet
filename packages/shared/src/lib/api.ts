2f30ea518780367284db6e7e3d3366fd939f844f
June <junederrill@gmail.com>
2025-09-11 00:04:58 +0100

    fuck it get 20 messages instead

--- packages/shared/src/lib/api.ts
+++ packages/shared/src/lib/api.ts
@@ -10,7 +10,7 @@ const Submission = z.object({
 
 export const getMessages = query(()=> {
     "use server"
-    return prisma.post.findMany({take: 10, orderBy: { id: 'desc'}, where: {visible: true}});
+    return prisma.post.findMany({take: 20, orderBy: { id: 'desc'}, where: {visible: true}});
 }, "getPosts")
 
 export const saveMessage = action(async (formData: FormData) => {
