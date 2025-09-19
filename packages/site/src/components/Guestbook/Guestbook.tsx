6ee77014bdc7dad0f6c1fe45bebfdb55ca4dc6c4
june <junederrill@gmail.com>
2025-09-19 13:49:35 +0100

    edit guestbook to scroll

--- packages/site/src/components/Guestbook/Guestbook.tsx
+++ packages/site/src/components/Guestbook/Guestbook.tsx
@@ -1,4 +1,4 @@
-import {Component, Suspense} from "solid-js";
+import {Component, createEffect, Suspense} from "solid-js";
 import {getMessages} from "shared/src/lib/api";
 import {createAsync} from "@solidjs/router";
 import styles from './Guestbook.module.scss'
@@ -7,14 +7,22 @@ import {GuestbookForm} from "~/components/GuestbookForm/GuestbookForm";
 export const Guestbook: Component = () => {
 
     const messages = createAsync(() => getMessages())
+    let messageDiv!: HTMLDivElement;
+
+    createEffect(() => {
+        messages()
+        if(messageDiv){
+            messageDiv.scrollTop = messageDiv.scrollHeight;
+        }
+    })
 
     return (
         <div class={styles.guestbook}>
             <div class={styles.container}>
                 <h3 class={"text-blue"}>Guestbook</h3>
-                <div class={styles.messages}>
+                <div class={styles.messages} ref={messageDiv}>
                     <Suspense>
-                        {messages()?.toSorted((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf()).map(message => (
+                        {messages()?.toSorted((a, b) => a.createdAt.valueOf() - b.createdAt.valueOf()).map(message => (
                             <div><span class={styles.author}>{message.author}</span>: {message.content}</div>
                         ))}
                     </Suspense>
