04329584a3a106744b7fedde014f65e29c56362c
June <junederrill@gmail.com>
2025-10-01 13:33:58 +0100

    update github link

--- packages/site/src/components/Links/Links.tsx
+++ packages/site/src/components/Links/Links.tsx
@@ -7,7 +7,7 @@ export const Links: Component = () => {
             <h2 class="text-blue">links</h2>
             <div class={styles.list}>
                 <a href="https://www.twitter.com/foppage">twitter</a>
-                <a href="https://www.github.com/june-rl">github</a>
+                <a href="https://www.github.com/foppage">github</a>
                 <a href="https://ch.tetr.io/u/june">tetr.io</a>
                 <a href="https://zudo.space">wife</a>
             </div>
