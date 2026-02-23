dd147775ccd4675ab84591a0af694f1719c510ce
June <me@june.pet>
2026-02-23 16:01:14 +0000

    add email/xmpp

--- packages/site/src/components/Links/Links.tsx
+++ packages/site/src/components/Links/Links.tsx
@@ -5,12 +5,15 @@ export const Links: Component = () => {
     return (
         <div class={styles.links + " text-normal"}>
             <h2 class="text-blue">links</h2>
-            <div class={styles.list}>
+            <p class={styles.list}>
                 <a href="https://www.twitter.com/foppage">twitter</a>
                 <a href="https://www.github.com/foppage">github</a>
                 <a href="https://bsky.app/profile/june.pet">bluesky</a>
                 <a href="https://isabelle.gg">wife</a>
-            </div>
+            </p>
+            <p>
+                email/xmpp: me@june.pet
+            </p>
         </div>
     )
 }
