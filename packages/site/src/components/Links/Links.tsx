4c815cd589b3a7aaecab526be962a75f0bc62c2b
June <me@june.pet>
2026-05-28 23:38:56 +0100

    change git link to my personal selfhosted git

--- packages/site/src/components/Links/Links.tsx
+++ packages/site/src/components/Links/Links.tsx
@@ -7,7 +7,7 @@ export const Links: Component = () => {
             <h2 class="text-blue">links</h2>
             <p class={styles.list}>
                 <a href="https://www.twitter.com/foppage">twitter</a>
-                <a href="https://www.github.com/foppage">github</a>
+                <a href="https://git.june.pet">git</a>
                 <a href="https://bsky.app/profile/june.pet">bluesky</a>
                 <a href="https://www.last.fm/user/foppage">last.fm</a>
                 <a href="https://isabelle.gg">wife</a>
